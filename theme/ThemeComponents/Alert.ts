9151ca4ae96f7f106b3be060d9119976572ee11a
hebaahmed400@gmail.com <hebaahmed400@gmail.com>
2024-11-24 00:12:23 +0200

    add color palette

--- theme/ThemeComponents/Alert.ts
+++ theme/ThemeComponents/Alert.ts
@@ -1,11 +1,36 @@
-import type { Theme } from '@mui/material/styles';
+import { colors } from "@mui/material";
+import type { Theme } from "@mui/material/styles";
 
 export const Alert = {
   MuiAlert: {
     styleOverrides: {
       root: ({ theme }: { theme: Theme }) =>
         theme.unstable_sx({
-          color: 'common.flush',
+          fontSize: "1rem",
+        }),
+
+      filledSuccess: ({ theme }: { theme: Theme }) =>
+        theme.unstable_sx({
+          color: "common.white",
+          backgroundColor: "success.main",
+        }),
+
+      filledError: ({ theme }: { theme: Theme }) =>
+        theme.unstable_sx({
+          color: "common.white",
+          backgroundColor: "error.main",
+        }),
+
+      filledInfo: ({ theme }: { theme: Theme }) =>
+        theme.unstable_sx({
+          color: "common.white",
+          backgroundColor: "info.main",
+        }),
+
+      filledWarning: ({ theme }: { theme: Theme }) =>
+        theme.unstable_sx({
+          color: "common.white",
+          backgroundColor: "warning.main",
         }),
     },
   },
